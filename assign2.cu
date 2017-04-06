be94bf6c6ce052c4f3f43c2df316fd15c3531f4f
Alan Plotko <alan.plotko@gmail.com>
2017-04-05 22:33:08 -0400

    Get U and V values

--- assign2.cu
+++ assign2.cu
@@ -33,6 +33,32 @@ void setGuards(std::ifstream &ifs, Point &a, Point &b, Point &c) {
     c = std::make_pair(x, y);
 }
 
+int getSPcores(cudaDeviceProp devProp) {
+    int cores = 0;
+    int mp = devProp.multiProcessorCount;
+    switch (devProp.major) {
+        case 2: // Fermi
+            if (devProp.minor == 1) cores = mp * 48;
+            else cores = mp * 32;
+            break;
+        case 3: // Kepler
+            cores = mp * 192;
+            break;
+        case 5: // Maxwell
+            cores = mp * 128;
+            break;
+        case 6: // Pascal
+            if (devProp.minor == 1) cores = mp * 128;
+            else if (devProp.minor == 0) cores = mp * 64;
+            else std::cerr << "Unknown device type\n";
+            break;
+        default:
+            std::cerr << "Unknown device type\n";
+            break;
+    }
+    return cores;
+}
+
 int main(int argc, char* argv[]) {
     // Process flags
     std::string filename;
@@ -78,19 +104,17 @@ int main(int argc, char* argv[]) {
     }
 
     // Use API to determine U and V values
-    int nDevices;
+    int nDevices, U, V;
     cudaError_t err = cudaGetDeviceCount(&nDevices);
     if (err != cudaSuccess) {
         std::cerr << cudaGetErrorString(err) << "\n";
+        exit(-1);
     } else {
         for (int i = 0; i < nDevices; i++) {
             cudaDeviceProp prop;
             cudaGetDeviceProperties(&prop, i);
-            std::cerr << "Device Number: " << i << "\n";
-            std::cerr << "  Device name: " << prop.name << "\n";
-            std::cerr << "  Memory Clock Rate (KHz): " << prop.memoryClockRate <<  "\n";
-            std::cerr << "  Memory Bus Width (bits): " << prop.memoryBusWidth <<  "\n";
-            std::cerr << "  Peak Memory Bandwidth (GB/s): " << 2.0*prop.memoryClockRate*(prop.memoryBusWidth/8)/1.0e6 << "\n";
+            U = prop.multiProcessorCount;
+            V = getSPcores(prop);
         }
     }
 
